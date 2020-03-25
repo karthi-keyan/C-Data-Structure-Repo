eea3abc1bea5abcf22c56fa26edad85b00fd1d25
karthikeyan <33755075+karthi-keyan@users.noreply.github.com>
2020-03-25 20:02:21 +0530

    Update sdkjghfjdg.cpp

--- sdkjghfjdg.cpp
+++ sdkjghfjdg.cpp
@@ -2,362 +2,305 @@
 
 using namespace std;
 
-typedef unsigned int u32;
 typedef unsigned short u16;
+typedef unsigned int  u32;
 
-struct package{
-
-    u32 pid;
-    u16 area,price,next=0;
-    bool isAlive=true;
-
+struct Message{
+    u32 msgID,time;
+    u16 userID,trieNext=0;
 };
 
-static u16 HashTable[150000];
+struct TrieNode{
+    u16 child[26]={0},message=0;
+};
 
-static u16 userToFriendList[1001][21];
-static u16 friendCounter[1001];
+static u16 userToUser[1000][1000];
+static u16 userCounter[1000];
 
-static u16 userArea[1001][11];
-static u16 userFriendArea[1001][12];
+static u16 userMessages[1000][100];
+static u16 userMessageCounter[1000];
 
-static u16 packageHeap[11][40001];
-static u16 heapCounter[11];
+static Message message[50001];
+static u16 messageCounter;
 
-static package packageList[40001];
-static u16 packageCounter=1;
+static TrieNode trie[40000];//5000*8<==>maximum hashtag * max length
+static u16 trieCounter;
 
-void init(u16 N,u16 M){
-    
-    packageCounter=1;
+int strToInt(char* str1){
 
-    for(int i=0;i<150000;i++){
+    int value=0;
 
-        HashTable[i]=0;
-        if(i<=40001)
-            packageList[i]={};
-
-        if(i<=1000){
-            for(int j=0;j<=20;j++)
-                userToFriendList[i][j]=0;
-            friendCounter[i]=1;
+    while(*str1!='_'){
+        if(*str1!=':'){
+            value=(value*10)+(*str1-48);
         }
-        if(i<=1000){
-            for(int j=0;j<11;j++){
-                userFriendArea[i][j]=0;
-                userArea[i][j]=0;
-            }
-            userFriendArea[i][11]=0;
-        }
+        ++str1;
     }
-    
-    for(int j=1;j<=10;j++){
-        for(int i=1;i<=40000;i++){
-                packageHeap[j][i]=0;
-        }
-        heapCounter[j]=1;
-    }
-}
-int getHash(int pid){
+    return value;
 
-    u32 hash = 5381,temp;
-    while(pid!=0){
-
-        temp=pid%10;
-        hash=((hash<<5)+hash)+temp;
-        pid/=10;
-
-    }
-    return hash%150000;
-}
-
-int hashTable(int pid){
-
-    int index = HashTable[getHash(pid)];
-    while(index!=0){
-
-        if(packageList[index].pid==pid)
-            return index;
-        
-        index=packageList[index].next;
-
-    }
-    return 0;
-}
-
-int parent(int index){
-    return index/2;
-}
-
-int left(int index){
-    return index*2;
-}
-
-int right(int index){
-    return index*2+1;
 }
 
 bool comparator(int index1,int index2){
 
-    if(packageList[index1].price<packageList[index2].price ||
-           (packageList[index1].price==packageList[index2].price && 
-            packageList[index1].pid<packageList[index2].pid))
-            return true;
+    if(message[index1].time>message[index2].time){
+        return true;
+    }
+    else if( message[index1].time==message[index2].time
+             && (message[index1].userID<message[index2].userID)){
+        return true;
+    }
     return false;
-
 }
 
-
-void addToHeap(int index_,int area){
+void insertIntoTrie(char* hashTag,int index){
     
-    int prnt;
-    u16 index = heapCounter[area];
-    packageHeap[area][index] = index_;
-    prnt = parent(index);
-    while(parent>0){
+    int trieIndex=0,alphaIndex,temp;
+    hashTag+=9;
+    while(*hashTag){
+        ++hashTag;
+        while(*hashTag!='_' && *hashTag ){
+            
+            alphaIndex = *hashTag - 97 ;
+            if(trie[trieIndex].child[alphaIndex]==0){
 
-        if(comparator(packageHeap[area][index],packageHeap[area][prnt])){
-            packageHeap[area][prnt] = packageHeap[area][prnt] ^ packageHeap[area][index];  
-            packageHeap[area][index] = packageHeap[area][prnt] ^ packageHeap[area][index];
-            packageHeap[area][prnt] = packageHeap[area][prnt] ^ packageHeap[area][index]; 
-  
-        }
-        else{
-            break;
-        }
-        
-        index = prnt;
-        prnt  = parent(index);
-
-    }
-
-    ++heapCounter[area];
-
-}
-
-void deleteFromHeap(int area){
-
-    int index,l,r,p,small=1;
-    --heapCounter[area];
-    index=heapCounter[area];
-    packageHeap[area][1]=packageHeap[area][index];
-    packageHeap[area][index]=0;
-    while(true){
-        
-        p = small;
-        r = right(p);
-        l = left(p);
-
-        if(l<index && comparator(packageHeap[area][l],packageHeap[area][p]))
-            small=l;
-        if(r<index && comparator(packageHeap[area][r],packageHeap[area][small]))
-            small=r;
-        if(small!=p){
-
-            packageHeap[area][p]     = packageHeap[area][p] ^ packageHeap[area][small];  
-            packageHeap[area][small] = packageHeap[area][p] ^ packageHeap[area][small];
-            packageHeap[area][p]     = packageHeap[area][p] ^ packageHeap[area][small]; 
-
-        }
-        else{
-            break;
-        }
-    }
-
-}
-
-void befriend(int uid1,int uid2){
-
-    userToFriendList[uid1][friendCounter[uid1]]=uid2;
-    ++friendCounter[uid1];
-    userToFriendList[uid2][friendCounter[uid2]]=uid1;
-    ++friendCounter[uid2];
-
-    for(int i=1;i<12;i++){
-
-        userFriendArea[uid1][i]+=userArea[uid2][i];
-        if(userFriendArea[uid1][i]>userFriendArea[uid1][11])
-            userFriendArea[uid1][11]=userFriendArea[uid1][i];
-        userFriendArea[uid2][i]+=userArea[uid1][i];
-        if(userFriendArea[uid2][i]>userFriendArea[uid2][11])
-            userFriendArea[uid2][11]=userFriendArea[uid2][i];
-
-    }
-}
-
-void add(int pid,int area,int price){
-
-    int temp,hash;
-    packageList[packageCounter].pid  =pid;
-    packageList[packageCounter].area =area;
-    packageList[packageCounter].price=price;
-
-    hash = getHash(pid);
-    temp = HashTable[hash];
-    HashTable[hash]=packageCounter;
-    packageList[packageCounter].next = temp;
-
-    addToHeap(packageCounter,area);
-
-    ++packageCounter;
-
-}
-
-void reserve(int uid,int pid){
-
-    int index = hashTable(pid);
-    int area  = packageList[index].area;
-    packageList[index].isAlive=false;
-    int frnd;
-
-    ++userArea[uid][area];
-
-    for(int i=1;i<friendCounter[uid];i++){
-       
-        frnd = userToFriendList[uid][i];
-        ++userFriendArea[frnd][area];
-        if(userFriendArea[frnd][area]>userFriendArea[frnd][11])
-            userFriendArea[frnd][11]=userFriendArea[frnd][area];
-        
-    }
-    ++userFriendArea[uid][area];
-    if(userFriendArea[uid][area]>userFriendArea[uid][11])
-        userFriendArea[uid][11]=userFriendArea[uid][area];
-
-}
-
-int recommend(int uid){
-
-    int maxArea = userFriendArea[uid][11];
-    bool visited =false;
-    u16 min_price=10000;
-    u32 min_id=2000000000;
-    
-    while(true){
-
-        for(int i=1;i<11;i++){
-
-            if(userFriendArea[uid][i]==maxArea){
-
-                while( packageHeap[i][1]!=0){
-                    
-                    int index=packageHeap[i][1];
-                    
-                    if(!packageList[index].isAlive)
-                        deleteFromHeap(i);
-
-                    else if( packageList[index].price<min_price ||
-                    (packageList[index].price==min_price&&
-                        packageList[index].pid<min_price)){
-
-                            min_price=packageList[index].price;
-                            min_id   =packageList[index].pid;
-                            visited=true;
-                            break;
-                        }
-                    else
-                        break;
-                }
-
+                trie[trieIndex].child[alphaIndex]=trieCounter;
+                trieIndex = trieCounter;
+                ++trieCounter;
+            }
+            else{
+                trieIndex = trie[trieIndex].child[alphaIndex];
             }
 
+            ++hashTag;
         }
-        if(visited)
-            break;
-        else{
-            --maxArea;
-        }
-
-    }
-
-    return min_id;
-
-}
-
-void printUtil(int area){
-
-    for(int i=0;i<11;i++){
-
-        cout<<packageList[packageHeap[area][i]].pid<<"\t";
-
-    }
-    cout<<endl;
-
-}
-
-void printFriend(int frnd){
-
-    for(int i=0;i<=11;i++){
-
-        cout<<userFriendArea[frnd][i]<<"\t";
-
-    }
-    cout<<endl;
+        if(*hashTag=='_')++hashTag;
+        temp = trie[trieIndex].message;
+        trie[trieIndex].message = index;
+        message[index].trieNext = temp;
+        trieIndex=0;
     
+    }
 }
 
+int searchFromTrie(char* hashTag){
+
+    int trieIndex=0,alphaIndex;
+    while(*hashTag){
+
+        alphaIndex = *hashTag - 97 ;
+        if(trie[trieIndex].child[alphaIndex]==0){
+            return 0;
+        }
+        else{
+            trieIndex = trie[trieIndex].child[alphaIndex];
+        }
+        ++hashTag;
+    }
+    return trie[trieIndex].message;
+}
+
+
+void init(){
+
+    messageCounter=trieCounter=1;
+    for(int i=0;i<1000;i++)
+        userCounter[i]= userMessageCounter[i] = 0;
+    for(int i=0;i<=50000;i++){
+        message[i]={};
+    }
+    for(int i=0;i<40000;i++){
+        trie[i]={};
+    }
+}
+
+void createMessage(int msgID,int userID,char msgData[]){
+
+    message[messageCounter].msgID  = msgID ; 
+    message[messageCounter].userID = userID;
+    message[messageCounter].time   = strToInt(msgData);
+
+    insertIntoTrie(msgData,messageCounter);
+    
+    userMessages[userID][userMessageCounter[userID]]=messageCounter;
+    
+    ++userMessageCounter[userID];
+    ++messageCounter;
+
+}
+
+void followUser(int userID1,int userID2){
+
+    userToUser[userID1][userCounter[userID1]]=userID2;
+    ++userCounter[userID1];
+
+}
+
+int searchByHashtag(char tagName[],int retIDs[]){
+
+    int counter=0,temp;
+    int result[]={0,0,0,0,0};
+    int index = searchFromTrie(tagName);
+    if(index==0){
+        return 0;
+    }
+    else{
+
+        while(index!=0){
+            
+            if(counter<4){
+                ++counter;
+                result[counter]=index;    
+            }
+            else if(comparator(index,result[4])){
+                result[counter]=index;
+            }
+            else{
+                continue;
+            }
+            temp=counter;
+            while(temp>0){
+                if(comparator(result[temp],result[temp-1])){
+                    result[temp]   = result[temp]^result[temp-1];
+                    result[temp-1] = result[temp]^result[temp-1];
+                    result[temp]   = result[temp]^result[temp-1];
+                }
+                else
+                    break;
+                temp--;
+            }
+            index=message[index].trieNext;
+        }
+    }
+    for(int i=0;i<5 && result[i]!=0;i++){
+        retIDs[i]=message[result[i]].msgID;
+    }
+    return counter;
+}
+
+int getMessages(int userID,int retIDs[]){
+
+    int counter=0,temp,index,user_temp=userID;
+    int result[]={0,0,0,0,0};
+    int following = userCounter[userID];
+
+    while(true){
+        if(userMessageCounter[userID]>0){
+            
+            for(int i=0;i<userMessageCounter[userID];i++){
+                
+                index=userMessages[userID][i];
+                if(counter<4){
+                    ++counter;
+                    result[counter]=index;    
+                }
+                else if(comparator(index,result[4])){
+                    result[counter]=index;
+                }
+                else{
+                    continue;
+                }
+                temp=counter;
+                while(temp>0){
+                    if(comparator(result[temp],result[temp-1])){
+                        result[temp]   = result[temp]^result[temp-1];
+                        result[temp-1] = result[temp]^result[temp-1];
+                        result[temp]   = result[temp]^result[temp-1];
+                    }
+                    else
+                        break;
+                    temp--;
+                }
+            }
+        }
+    
+        if(following<=0){
+            break;
+        }
+        else{
+           userID=userToUser[user_temp][following-1];
+           --following;
+        }
+    }
+
+    for(int i=0;i<5 && result[i]!=0;i++){
+        retIDs[i]=message[result[i]].msgID;
+    }
+    return counter;
+}
+
+void printUtilTrie(){
+
+    cout<<endl;
+    for(int i=0;i<15;i++){
+
+        for(int j=0;j<26;j++){
+    
+            cout<<trie[i].child[j]<<' ';
+
+        }
+        cout<<endl<<trie[i].message<<endl;
+
+    }
+
+}
+
+void printUtilMess(){
+
+    cout<<endl;
+    for(int i=0;i<15;i++){
+
+        cout<<message[i].userID<<"  "<<message[i].msgID<<"  "<<message[i].time;
+        cout<<endl;
+
+    }
+
+}
+
+void printUser(){
+
+    for(int i=1;i<5;i++){
+
+        for(int j=0;j<userCounter[i];j++){
+            
+            cout<<userToUser[i][j]<<" ";
+
+        }
+        cout<<endl;
+        break;
+    }
+}
 
 int main(){
-    init(5,4);
-    befriend(1,2);
-    befriend(1,3);
-    befriend(1,4);
-    befriend(5,3);
-    befriend(2,3);
-
-    add(11,1,55);
-    add(12,1,34);
-    add(13,1,544);
-    add(15,1,54);
-    add(14,1,54);
-    add(16,2,45);
-    add(2,3,564);
-    add(67,2,5);
-    add(9,2,67);
-
-    printUtil(1);
-    printUtil(2);
-    printUtil(3);
-
-
-    cout<<recommend(1)<<endl;
-    cout<<recommend(2)<<endl;
-    cout<<recommend(3)<<endl;
-    cout<<recommend(4)<<endl;
-    cout<<recommend(5)<<endl;
-
-    reserve(1,12);
-    reserve(1,14);
-    reserve(1,13);
-    reserve(1,11);
-    reserve(1,15);
-    reserve(1,67);
-    reserve(1,16);
-    reserve(1,9);
-
-    printUtil(1);
-    printUtil(2);
-    printUtil(3);
-
-    cout<<"\n\n";
-
-    printFriend(2);
-    printFriend(3);
-    printFriend(4);
-    printFriend(5);
-
-    cout<<recommend(2)<<endl;
-    cout<<recommend(3)<<endl;
-    cout<<recommend(4)<<endl;
     
-    printUtil(1);
-    printUtil(2);
-    printUtil(3);
+    init();
+    
+    createMessage(1111,1,"00:32:45_#parrot_#papa");
+    createMessage(2222,2,"23:32:45_#party");
+    createMessage(3333,3,"23:32:45_#papa");
+    createMessage(4444,4,"23:32:45_#pop");
+    createMessage(5555,5,"23:32:45_#p");
 
-    cout<<"\n\n";
-    befriend(1,5);
-    printFriend(5);
+    printUtilMess();
+    //printUtilTrie();
 
+    followUser(1,2);
+    followUser(1,3);
+    followUser(1,5);
+    followUser(2,3);
 
+    printUser();
+
+    int result[]={0,0,0,0,0};
+
+    //searchByHashtag("papadsfhjk",result);
+
+    getMessages(6,result);
+
+    for(int i=0;i<=4;i++){
+
+        cout<<result[i]<<" "<<endl;
+
+    }
 
 }
+
